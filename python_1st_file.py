d69d13eb217dd5f4ddec329279f990f86e13fea3
Soumojit Saha <34377460+wbsoumo@users.noreply.github.com>
2022-07-31 12:40:19 +0530

    Create python_1st_file.py

---
+++ python_1st_file.py
@@ -0,0 +1,12 @@
+print("Hello world!")
+print("Congratulation!!!! soumojit this is your first code")
+
+#Hello this is my first code with visual studio
+
+num1=200
+num2=500
+sum = num1+num2
+
+print('The sum of {0} and {1} is {2}' .format(num1 , num2, sum))
+
+print("Test text")
